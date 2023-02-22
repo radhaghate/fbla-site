136f17d6cf11655bf01a8bf1a4d437558f63ea1a
Nikhil Ghate <nickinnj44@gmail.com>
2023-02-21 21:33:30 -0500

    sandbox

---
+++ src/pages/sandbox.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      Hi Radha :)
+    </div>
+  );
+}
+
+export default Home;
